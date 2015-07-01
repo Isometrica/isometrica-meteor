2fde1b529414e6dde93cae98e85b94c80e4dcd6c
stephen fortune <steve.fortune@icecb.com>
2015-07-01 15:56:28 +0100

    feature(address-book): Started porting obscure numbers function over - might leave this for now in favour of getting the frontend working

--- model/callTreeNode.js
+++ model/callTreeNode.js
@@ -1,3 +1,30 @@
 CallTreeNode = new Mongo.Collection("callTreeNode");
 
 'use strict';
+
+/**
+ * @note In future there will be more complex access control here..
+ */
+CallTreeNode.allow({
+  remove: function() {
+    return true;
+  }
+});
+
+Meteor.methods({
+
+  /**
+   * Add a new node for the user of a given id.
+   *
+   * @todo Validation
+   * @todo Check that the node doesn't already exist for the user
+   * @todo Check that the node isn't _for_ the user
+   * @todo Check that the user exists
+   */
+  add: function(userId, node) {
+    CallTreeNode.insert(_.extend(node, {
+      owner: userId
+    }));
+  }
+
+});
--- model/contacts.js
+++ model/contacts.js
@@ -1,3 +1,12 @@
 Contacts = new Mongo.Collection("contacts");
 
 'use strict';
+
+CallTreeNode.allow({
+});
+
+Meteor.methods({
+  updateContact: function() {
+
+  }
+});
