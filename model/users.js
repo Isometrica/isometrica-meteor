ab81171b668908b218707328be05ab1647c5b4c3
stephen fortune <steve.fortune@icecb.com>
2015-07-09 12:20:25 +0100

    feature(address-book): #21 #18 Setup some models
    - Phone number schema
    - Membership collection
    - Superpower schema

--- model/users.js
+++ model/users.js
@@ -2,6 +2,15 @@
 
 var Users = Meteor.users;
 
+var emailExistsValidator = function() {};
+
+UserProfile = new SimpleSchema({
+  firstName: {},
+  lastName: {},
+  phoneNumbers: {},
+  email: {}
+});
+
 Users._transform = function(doc) {
   if (doc.profile) {
     _.extend(doc.profile, {
