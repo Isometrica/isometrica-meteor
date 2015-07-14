aceb43f8b27f918aa0f2d0a00d4d1def3ec0fe4f
stephen fortune <steve.fortune@icecb.com>
2015-07-14 15:35:05 +0100

    feature)address-book): Figured out why the address book guidance header was misaligned; used hack from old codebase

--- client/app/app.js
+++ client/app/app.js
@@ -18,6 +18,22 @@ var app = angular.module('isa', [
       $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
           console.log("StateChangeError: ", error);
       });
+  }])
+  .run(['$rootScope', '$window', function($rootScope, $window) {
+      $rootScope.Schemas = $window.Schemas;
+  }])
+  .run(['$rootScope', function($rootScope) {
+    // Taken from:
+    // https://bitbucket.org/markleusink/isometrica/src/f687698336fbb26e5b56a2422d9137245a5d57c7/client/app/controller.js?at=develop#cl-14
+    //
+    var body = angular.element(document.querySelector('body'));
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+      if (toState.name == 'account.overview' || toState.name == 'overview' || toState.name == 'welcome') {
+        body.addClass('has-bootcards-navbar-double');
+      } else {
+        body.removeClass('has-bootcards-navbar-double');
+      }
+    });
   }]);
 
 var boot = function() {
