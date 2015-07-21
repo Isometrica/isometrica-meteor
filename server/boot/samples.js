14dfad02db35cd2372245054b1ca69c2e166b702
stephen fortune <steve.fortune@icecb.com>
2015-07-21 12:33:45 +0100

    feature(core): Updated sample script to create a consultant as well

--- server/boot/samples.js
+++ server/boot/samples.js
@@ -3,43 +3,67 @@
 Meteor.startup(function() {
 
   var log = console.log;
-  var organisation = {
-    name: "Teamstudio"
+
+  var consultant = {
+    profile: {
+      firstName: 'Mark',
+      lastName: 'Leusink'
+    },
+    password: 'password123',
+    email: 'mark@linqed.eu'
   };
-  var users = [
-    {
-      profile: {
-        firstName: 'Mark',
-        lastName: 'Leusink'
+
+  var zetaComm = {
+    name: "ZetaComm",
+    users: [
+      {
+        profile: {
+          firstName: 'User',
+          lastName: 'One'
+        },
+        password: 'password123',
+        email: 'user1@zeta.com'
       },
-      password: 'password123',
-      email: 'mark@linqed.eu'
-    },
-    {
-      profile: {
-        firstName: 'Steve',
-        lastName: 'Fortune'
+      {
+        profile: {
+          firstName: 'User',
+          lastName: 'Two'
+        },
+        password: 'password123',
+        email: 'user2@zeta.com'
+      }
+    ]
+  };
+
+  var teamstudio = {
+    name: "Teamstudio",
+    users: [
+      {
+        profile: {
+          firstName: 'Steve',
+          lastName: 'Fortune'
+        },
+        password: 'password123',
+        email: 'steve.fortune@icecb.com'
       },
-      password: 'password123',
-      email: 'steve.fortune@icecb.com'
-    },
-    {
-      profile: {
-        firstName: 'Michael',
-        lastName: 'Hamilton'
+      {
+        profile: {
+          firstName: 'Michael',
+          lastName: 'Hamilton'
+        },
+        password: 'password123',
+        email: 'michael@teamstudio.com'
       },
-      password: 'password123',
-      email: 'michael@teamstudio.com'
-    },
-    {
-      profile: {
-        firstName: 'Steve',
-        lastName: 'Ives'
-      },
-      password: 'password123',
-      email: 'steve@teamstudio.com'
-    }
-  ];
+      {
+        profile: {
+          firstName: 'Steve',
+          lastName: 'Ives'
+        },
+        password: 'password123',
+        email: 'steve@teamstudio.com'
+      }
+    ]
+  }
 
   var canAddSamples = function(cb) {
     if (!(Meteor.users.find().count() || process.env.IS_MIRROR)) {
@@ -51,12 +75,21 @@ Meteor.startup(function() {
   // Although we should be able to use a Mongo Collection driver directly..
   canAddSamples(function() {
     log('Creating sample data');
-    var orgId = Organisations.insert(organisation);
-    Partitioner.bindGroup(orgId, function() {
-      _.each(users, function(user) {
-        log('User: ' + user.profile.firstName + ' ' + user.profile.lastName);
-        Meteor.call("registerOrganisationUser", user, orgId);
-      });      
+    var consultantId = Meteor.call("registerUser", consultant);
+    _.each([ teamstudio, zetaComm ], function(org) {
+      var orgId = Organisations.insert({
+        name: org.name
+      });
+      Partitioner.bindGroup(orgId, function() {
+        _.each(org.users, function(user) {
+          log('Adding ' + user.profile.firstName + ' ' + user.profile.lastName + ' to ' + org.name);
+          Meteor.call("registerOrganisationUser", user);
+        });
+        Memberships.insert({
+          userId: consultantId,
+          isAccepted: true
+        });
+      });
     });
   });
 
