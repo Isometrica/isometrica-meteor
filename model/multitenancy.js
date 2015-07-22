988c4c28a78ad7be932aa9d1c440e20cb74a2005
stephen fortune <steve.fortune@icecb.com>
2015-07-22 16:46:44 +0100

    feature(core): Progression with multitenancy - setup initial collection hooks

--- model/multitenancy.js
+++ model/multitenancy.js
@@ -41,36 +41,123 @@ MultiTenancy = {};
 
 'use strict';
 
+var assertClient = function() {
+  if (Meteor.isServer) {
+    throw new Meteor.Error(404, 'Only available on the client');
+  }
+};
+
+var assertServer = function() {
+  if (!Meteor.isServer) {
+    throw new Meteor.Error(404, 'Only available on the server');
+  }
+};
+
 /**
  * @constructor
- * Paraciticly proxies Mongo.Collection ctor. Applies multi-tenancy hooks
- * to the collection.
+ * Proxies Mongo.Collection ctor. Applies multi-tenancy hooks to the
+ * collection.
  *
+ * @note  No update / remove hooks are required. find / findOne are
+ *        called before update and remove anyway.
+ * @host  Server | Client
  * @param name String
  * @see   Mongo.Collection
  */
 MultiTenancy.Collection = function(name) {
+
   var col = new Mongo.Collection(name);
+  var constrainFind;
+  var constrainInsert;
+
+  var assertUser = function(userId) {
+    if (!userId) {
+      throw new Meteor.Error(403, 'Login required to access multi-tenancy collection');
+    }
+  };
+
+  if (Meteor.isServer) {
+
+    var findOrgIds = function(userId) {
+      return MultiTenancy.memberships.find({
+        userId: userId
+      }, {
+        _orgId: 1
+      }).fetch().map(function(mem) {
+        return mem._orgId;
+      });
+    };
+
+    constrainFind = function(userId, sel) {
+      sel._orgId = {
+        $in: findOrgIds(userId)
+      };
+    };
+
+    constrainInsert = function(userId, doc) {
+      if (!doc._orgId) {
+        throw new Meteor.Error(403, 'No orgId! Make sure that have configured the client correctly');
+      }
+      var orgIds = findOrgIds(userId);
+      if(!~orgIds.indexOf(doc._orgId)) {
+        throw new Meteor.Error(403, 'User does not have access to this organisation');
+      }
+    };
+
+  } else {
+
+    constrainFind = function(userId, sel) {
+      sel._orgId = MultiTenancy.orgId();
+    };
+
+    constrainInsert = function(user, doc) {
+      if (!MultiTenancy.orgId()) {
+        throw new Meteor.Error(403, 'Set MultiTenancy.orgId()');
+      }
+      doc._orgId = MultiTenancy.orgId();
+    }
+
+  }
+
+  col.before.all(assertUser);
+  col.before.insert(constrainInsert);
+  col.before.find(constrainFind);
+  col.before.findOne(constrainFind);
+
   return col;
+
 };
 
 /**
+ * Get / set the current org id on the client.
  *
+ * @param orgId falsy | String
+ * @host Client
  */
-MultiTenancy.organisation = function() {
-
+MultiTenancy.orgId = function(orgId) {
+  assertClient();
+  var key = 'MultiTenancy.orgId';
+  if (orgId) {
+    return Session.get(key);
+  } else {
+    Session.set(key, orgId);
+  }
 };
 
 /**
- * Collection of different host organisations.
+ * Collection of different organisations.
  *
+ * @todo What on the client ?
+ * @host Client | Server
  * @var Mongo.Collection
  */
 MultiTenancy.organisations = new Mongo.Collection('organisations');
 
 /**
- * Partitioned collection of memberships to different hosts.
+ * Partitioned collection of memberships of different organisations.
  *
+ * @todo What on the client ?
+ * @host Client | Server
  * @var MultiTenancy.Collection
  */
 MultiTenancy.memberships = new MultiTenancy.Collection('memberships');
