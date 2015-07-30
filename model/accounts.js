df2317b3c3ae60000e02596b54cac0ab24400c92
stephen fortune <steve.fortune@icecb.com>
2015-07-30 16:08:39 +0100

    feature(account-settings): Setup initial account setting schema and sub-schemas

---
+++ model/accounts.js
@@ -0,0 +1,62 @@
+
+Accounts = new Mongo.Collection("accounts");
+
+'use strict';
+
+Schemas.InvoicingStatus = new SimpleSchema([Schemas.IsaBase, {
+  manualInvoicingState: {
+    type: String,
+    allowedValues: [
+      "none",
+      "requested",
+      "approved"
+    ]
+  }
+}]);
+
+Schemas.BillingDetails = new SimpleSchema({
+  email: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email,
+  },
+  address: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  zip: {
+    type: String
+  },
+  country: {
+    type: String
+  }
+});
+
+Schemas.Account = new SimpleSchema([Schemas.IsaBase, {
+  organisationName: {
+    type: String
+  },
+  maxStorageMB: {
+    type: Number
+  },
+  manualInvoicing: {
+    type: Schemas.InvoicingStatus
+  },
+  defaultLanguage: {
+    type: String,
+    allowedValues: [
+      'en-GB',
+      'en-US'
+    ]
+  },
+  isPublicTemplateAdmin: {
+    type: Boolean,
+    defaultValue: false
+  },
+  billingDetails: {
+    type: Schemas.BillingDetails
+  }
+}]);
+
+Accounts.attachSchema(Schemas.Account);
