c54c5e3b33a4bd37fa05e2086ff280f48e055715
Mark Leusink <m.leusink@gmail.com>
2015-08-05 15:56:49 +0200

    docwiki and files: styling

--- client/app/utils.js
+++ client/app/utils.js
@@ -9,17 +9,17 @@ isa.utils = {
 
 		switch (ext) {
 			case 'pdf':
-				return 'fa fa-file-pdf-o';
+				return 'fa fa-lg fa-file-pdf-o';
 			case 'doc': case 'docx':
-				return 'fa fa-file-word-o';
+				return 'fa fa-lg fa-file-word-o';
 			case 'ppt': case 'pptx':
-				return 'fa fa-file-powerpoint-o';
+				return 'fa fa-lg fa-file-powerpoint-o';
 			case 'xls': case 'xlsx':
-				return 'fa fa-file-excel-o';
+				return 'fa fa-lg fa-file-excel-o';
 			case 'jpg': case 'jpeg':
-				return 'fa fa-file-picture-o';
+				return 'fa fa-lg fa-file-picture-o';
 			default:
-				return 'fa fa-file-text-o';
+				return 'fa fa-lg fa-file-text-o';
 		}
 
 	},
