bf3e038ce7699e53fd3f61f3b4a6815699128074
stephen fortune <steve.fortune@icecb.com>
2015-08-14 17:58:26 +0100

    feature(address-book): Separated out read / writing phone numbers into its own directive
    - New isaPhoneNumbers directive.
    - There are both edit and view templates for this directive; use the readonly attr to determine which
      one is renderedd

---
+++ client/addressBook/directive/phoneNumbers.ng.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular
+  .module('isa.addressbook')
+  .directive('isaPhoneNumbers', isaPhoneNumbersDirective);
+
+function isaPhoneNumbersDirective() {
+  return {
+    restrict: 'E',
+    templateUrl: function(elm, attrs) {
+      var tmpPrefix = (attrs.readonly ? 'view' : 'edit');
+      return 'client/addressBook/view/' + tmpPrefix + 'PhoneNumbers.ng.html';
+    },
+    controller: function($scope) {
+      $scope.addPhoneNumber = function() {
+        $scope.contactable.phoneNumbers.push({});
+      }
+      $scope.removePhoneNumber = function(index) {
+        $scope.contactable.phoneNumbers.splice(index, 1);
+      }
+    },
+    scope: {
+      contactable: '=',
+      readonly: '@'
+    }
+	};
+}
