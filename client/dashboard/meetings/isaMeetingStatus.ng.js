88768634dda82268f7532182b5384f4dbaf18f00
Michael Hamilton <michael_hamilton@teamstudio.com>
2015-08-24 14:16:19 -0400

    meetings: update action status indicators to latest spec

--- client/dashboard/meetings/isaMeetingStatus.ng.js
+++ client/dashboard/meetings/isaMeetingStatus.ng.js
@@ -17,16 +17,59 @@ function isaMeetingStatusDirective() {
       return;
     }
 
+    var type = attr.type || 'circle';
+
     elem.addClass('label');
-    elem.addClass('status-indicator');
-    if (!mtg.actions || 0 === mtg.actions.length) {
-      if (optional) {
-        elem.hide();
+    if ('circle' === type) {
+      elem.addClass('status-indicator');
+    }
+
+    scope.$watch(function() {
+      var actions = MeetingActions.find({meetingId: mtg._id, inTrash: false}).fetch();
+      if (!actions || 0 === actions.length) {
+        if (optional) {
+          elem.hide();
+        }
       }
-    }
-    else {
-      elem.show();
-      // TODO: style based on actions
-    }
+      else {
+        elem.show();
+        var state = 0;
+        var openCount = 0;
+        var overdueCount = 0;
+        _.each(actions, function(action) {
+          if (action.status == 'open' && moment(action.targetDate).isBefore(new Date())) {
+            state = 2;
+            ++overdueCount;
+          }
+          else if (action.status == 'open' && state < 2) {
+            state = 1;
+            ++openCount;
+          }
+        });
+
+        elem.addClass(['label-success', 'label-warning', 'label-danger'][state]);
+        if ('label' === type) {
+          var html = null;
+          switch (state) {
+            case 0:
+              html = 'No open actions';
+              break;
+            case 1:
+              html = openCount + ' open action';
+              if (openCount > 1) {
+                html += 's';
+              }
+              break;
+            case 2:
+              html = overdueCount + ' overdue action';
+              if (overdueCount > 1) {
+                html += 's';
+              }
+              break;
+          }
+          elem.html(html);
+        }
+      }
+    });
   }
 }
