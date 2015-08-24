03313e9d73fcf68b6cd89cd92ffd8fc2dc14ab9a
stephen fortune <steve.fortune@icecb.com>
2015-08-24 17:17:58 +0100

    Hotfix - attempt to hotfix eProvider error with isaPhoto field

--- client/addressBook/form/isaPhoto.ng.js
+++ client/addressBook/form/isaPhoto.ng.js
@@ -4,6 +4,23 @@ angular
 	.module('isa.addressbook')
 	.config(isaPhoto);
 
+function isaPhotoController($scope, $rootScope) {
+	var images = $scope.$meteorCollectionFS(IsaProfileImages, false);
+	$scope.upload = function(files) {
+		if (!files.length) {
+			return;
+		}
+		images.save(files).then(function(res) {
+			$scope.file = res[0]._id;
+			$scope.model[$scope.options.key] = {
+				_id: $scope.file._id,
+				name: $scope.file.name(),
+				size: $scope.file.size()
+			};
+		});
+	};
+}
+
 function isaPhoto(formlyConfigProvider) {
   formlyConfigProvider.setType({
     name: 'isaPhoto',
@@ -11,22 +28,7 @@ function isaPhoto(formlyConfigProvider) {
     defaultOptions: {
       className: 'form-group avatar'
     },
-    controller: function($scope, $rootScope) {
-      var images = $scope.$meteorCollectionFS(IsaProfileImages, false);
-			$scope.upload = function(files) {
-				if (!files.length) {
-					return;
-				}
-				images.save(files).then(function(res) {
-					$scope.file = res[0]._id;
-					$scope.model[$scope.options.key] = {
-						_id: $scope.file._id,
-						name: $scope.file.name(),
-						size: $scope.file.size()
-					};
-				});
-			};
-    },
+    controller: isaPhotoController,
     wrapper: [ 'hzLabel', 'bootstrapHasError' ]
   });
 }
