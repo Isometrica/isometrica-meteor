199ade485676b2958a3a73ed7e7cc9f7f099ce59
stephen fortune <steve.fortune@icecb.com>
2015-09-01 16:27:08 +0100

    feature(address-book): New contact now brings up the detail card instead of the unary field card

--- client/addressBook/controller/addressBookController.ng.js
+++ client/addressBook/controller/addressBookController.ng.js
@@ -80,7 +80,7 @@ function AddressBookController($scope, $rootScope, $state, $modal, $meteor, orga
 			route: 'addressbook.contact',
 			collection: $scope.$meteorCollection(Contacts, false).subscribe("contacts"),
 			modalControllerConf: {
-				templateUrl: 'client/addressBook/view/newContact.ng.html',
+				templateUrl: 'client/addressBook/view/editContact.ng.html',
 				controller : 'AddressBookEditContactController'
 			}
 		},
@@ -90,7 +90,7 @@ function AddressBookController($scope, $rootScope, $state, $modal, $meteor, orga
 				.$meteorCollection(OrganisationAddresses, false)
 				.subscribe('organisationAddresses'),
 			modalControllerConf: {
-				templateUrl: 'client/addressBook/view/newOrganisationAddress.ng.html',
+				templateUrl: 'client/addressBook/view/editOrganisationAddress.ng.html',
 				controller : 'AddressBookEditOrganisationAddressController'
 			}
 		}
