9bd7273888d8a9458019ba38dc8ad4b5455d3562
Stephen Fortune <steve.fortune@icecb.com>
2015-09-09 11:48:45 +0100

    feature(address-book): Removed old row aggregating functions and tabs->spaces

--- client/addressBook/controller/editUserController.ng.js
+++ client/addressBook/controller/editUserController.ng.js
@@ -32,189 +32,177 @@ function AddressBookGroupFilter() {
  */
 function AddressBookEditUserController($scope, $rootScope, $modalInstance, $modal, $controller, $meteor, object, growl) {
 
-	$controller('AddressBookEditController', {
-		$scope: $scope,
-		$modalInstance: $modalInstance,
-		collection: angular.noop,
-		object: object
-	});
+  $controller('AddressBookEditController', {
+    $scope: $scope,
+    $modalInstance: $modalInstance,
+    collection: angular.noop,
+    object: object
+  });
 
-	/**
-	 * Local user collection.
-	 *
-	 * @var AngularMeteorCollection
-	 */
-	var users = $scope.$meteorCollection(Meteor.users, false);
+  /**
+   * Local user collection.
+   *
+   * @var AngularMeteorCollection
+   */
+  var users = $scope.$meteorCollection(Meteor.users, false);
 
-	/**
-	 * Local memberships collection.
-	 *
-	 * @var AngularMeteorCollection
-	 */
-	$scope.memberships = $scope.$meteorCollection(Memberships, false);
+  /**
+   * Local memberships collection.
+   *
+   * @var AngularMeteorCollection
+   */
+  $scope.memberships = $scope.$meteorCollection(Memberships, false);
 
-	if ($scope.isNew) {
+  if ($scope.isNew) {
 
-		/**
-		 * New, empty membership !
-		 *
-		 * @var Object
-		 */
-		$scope.membership = {};
+    /**
+     * New, empty membership !
+     *
+     * @var Object
+     */
+    $scope.membership = {};
 
-		/**
-		 * Clean so that default and autoValues are populated for
-		 * new user object.
-		 */
-		Schemas.User.clean($scope.object);
-		Schemas.Membership.clean($scope.membership);
+    /**
+     * Clean so that default and autoValues are populated for
+     * new user object.
+     */
+    Schemas.User.clean($scope.object);
+    Schemas.Membership.clean($scope.membership);
 
-		/**
-		 * @protected
-		 * @override
-		 */
-		$scope.save = function() {
-			$scope.loading = true;
-			$meteor
-				.mtCall('registerOrganisationUser', $scope.object, $scope.membership)
-				.then($scope.success, $scope.failure);
-		};
+    /**
+     * @protected
+     * @override
+     */
+    $scope.save = function() {
+      $scope.loading = true;
+      $meteor
+        .mtCall('registerOrganisationUser', $scope.object, $scope.membership)
+        .then($scope.success, $scope.failure);
+    };
 
-	} else {
+  } else {
 
-		/**
-		 * Creates a copy of the existing object with key attributes
-	 	 * having been rearrange to the appropriate position in the
-		 * document.
-		 *
-		 * @return Object
-		 */
-		var createPayload = function() {
-			var user = angular.copy($scope.object);
-			user.emails[0].address = user.email;
-			delete user.email;
-			return user;
-		};
+    /**
+     * Creates a copy of the existing object with key attributes
+      * having been rearrange to the appropriate position in the
+     * document.
+     *
+     * @return Object
+     */
+    var createPayload = function() {
+      var user = angular.copy($scope.object);
+      user.emails[0].address = user.email;
+      delete user.email;
+      return user;
+    };
 
-		/**
-		 * Existing membership to manipulate.
-		 *
-		 * @var Object
-		 */
-		$scope.membership = Memberships.findOne({
-			userId: object._id
-		});
+    /**
+     * Existing membership to manipulate.
+     *
+     * @var Object
+     */
+    $scope.membership = Memberships.findOne({
+      userId: object._id
+    });
 
-		var inArr = function(arr) {
-			return arr && !!_.find(arr, function(obj) {
-				return obj._id === $scope.object._id;
-			});
-		};
+    var inArr = function(arr) {
+      return arr && !!_.find(arr, function(obj) {
+        return obj._id === $scope.object._id;
+      });
+    };
 
-		var isOwner = function(doc) {
-			return doc.owner._id === $scope.object._id;
-		}
+    var isOwner = function(doc) {
+      return doc.owner._id === $scope.object._id;
+    }
 
-		/**
-		 * Local docwiki collection - all documents and the access levels
-		 * that the user in question has for them.
-		 *
-		 * @var AngularMeteorCollection
-		 */
-		$scope.docs = $scope.$meteorCollection(function() {
-			return Modules.find({
-				inTrash: false,
-				isArchived: false,
-				isTemplate: false
-			}, {
-				transform: function(doc) {
-					var permission;
-					if (isOwner(doc)) {
-						permission = "Owner";
-					} else {
-						var accessOverrides = {
-							"Reader": doc.allowReadByAll,
-							"Editor": doc.allowEditByAll
-						};
-						var predicate = function(accessObj) {
-							return accessOverrides[accessObj.name] || inArr(accessObj.col);
-						};
-						var permissions = _.map(_.filter([
-							{ name: "Editor", col: doc.editors },
-							{ name: "Reader", col: doc.readers },
-							{ name: "Signer", col: doc.signers },
-							{ name: "Approver", col: doc.approvers }
-						], predicate), function(obj) {
-							return obj.name
-						});
+    /**
+     * Local docwiki collection - all documents and the access levels
+     * that the user in question has for them.
+     *
+     * @var AngularMeteorCollection
+     */
+    $scope.docs = $scope.$meteorCollection(function() {
+      return Modules.find({
+        inTrash: false,
+        isArchived: false,
+        isTemplate: false
+      }, {
+        transform: function(doc) {
+          var permission;
+          if (isOwner(doc)) {
+            permission = "Owner";
+          } else {
+            var accessOverrides = {
+              "Reader": doc.allowReadByAll,
+              "Editor": doc.allowEditByAll
+            };
+            var predicate = function(accessObj) {
+              return accessOverrides[accessObj.name] || inArr(accessObj.col);
+            };
+            var permissions = _.map(_.filter([
+              { name: "Editor", col: doc.editors },
+              { name: "Reader", col: doc.readers },
+              { name: "Signer", col: doc.signers },
+              { name: "Approver", col: doc.approvers }
+            ], predicate), function(obj) {
+              return obj.name
+            });
 
-						if (permissions.length === 0) {
-							permission = "No Access";
-						} else {
-							permission = permissions.join(" + ");
-						}
-					}
-					return angular.extend(doc, {
-						permission: permission
-					});
-				}
-			});
-		}, false).subscribe('modules');
+            if (permissions.length === 0) {
+              permission = "No Access";
+            } else {
+              permission = permissions.join(" + ");
+            }
+          }
+          return angular.extend(doc, {
+            permission: permission
+          });
+        }
+      });
+    }, false).subscribe('modules');
 
-		/**
-		 * @protected
-		 * @override
-		 */
-		$scope.save = function() {
-			$scope.loading = true;
-			var payload = createPayload();
-			users.save(payload).then(function() {
-				return $scope.memberships.save($scope.membership);
-			}).then($scope.success, $scope.failure);
-		};
+    /**
+     * @protected
+     * @override
+     */
+    $scope.save = function() {
+      $scope.loading = true;
+      var payload = createPayload();
+      users.save(payload).then(function() {
+        return $scope.memberships.save($scope.membership);
+      }).then($scope.success, $scope.failure);
+    };
 
-		/**
-		 * Sends reset password email to the user.
-		 */
-		$scope.changePassword = function() {
-			$scope.loading = true;
-			$meteor.call('resetUserPassword', $scope.object._id).then(function() {
-				growl.info('A password reset email has been sent to this user.');
-				$scope.loading = false;
-			}, $scope.failure);
-		};
+    /**
+     * Sends reset password email to the user.
+     */
+    $scope.changePassword = function() {
+      $scope.loading = true;
+      $meteor.call('resetUserPassword', $scope.object._id).then(function() {
+        growl.info('A password reset email has been sent to this user.');
+        $scope.loading = false;
+      }, $scope.failure);
+    };
 
+    /**
+     * Set the id of a membership that the user's resources are transferred to on
+     * delete.
+     *
+     * @param id
+     */
     $scope.setDstId = function(id) {
       $scope.dstMemId = id;
     };
 
-    var rowItems = 3;
-
-    $scope.membershipRowAt = function(index) {
-      var start = index*rowItems;
-      var end = start + (rowItems - 1);
-      console.log('Row index: ' + index);
-      console.log('Start: ' + start + ', end: ' + end);
-      var mems = $scope.memberships.slice(start, end);
-      console.log('Membership row', mems);
-      return mems;
-    };
 
     /**
+     * Transform the object such that the email key exists for editing
+     * directily in the object.
      *
+     * @see createPayload
      */
-    $scope.deleteMembership = function() {
+    $scope.object.email = $scope.object.emails[0].address;
 
-    };
-
-		/**
-		 * Transform the object such that the email key exists for editing
-		 * directily in the object.
-		 *
-		 * @see createPayload
-		 */
-		$scope.object.email = $scope.object.emails[0].address;
-
-	}
+  }
 
 }
