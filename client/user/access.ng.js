2cfd56757b546d19f33d2a0a4c97a730aa6eb413
Stephen Fortune <steve.fortune@icecb.com>
2015-09-16 12:42:06 +0100

    feature(address-book): isaSuperpowers can now be a data-bound object

--- client/user/access.ng.js
+++ client/user/access.ng.js
@@ -13,19 +13,24 @@ function isaAccess($compile) {
   return {
     restrict: 'EA',
     transclude: true,
-    template: '<fieldset ng-disabled="denied() || isaLoading" ng-transclude></fieldset>',
+    template: '<fieldset ng-disabled="denied || isaLoading" ng-transclude></fieldset>',
     link: function(scope, elm, attrs, isaAccessCtrl) {
-      if (scope.isaSuperpowers && scope.isaSuperpowers.length) {
+      if (scope.isaSuperpowers) {
         var mem = scope.$meteorObject(Memberships, {
           userId: scope.$root.currentUser._id
         });
-        scope.denied = function() {
-          return !_.every(scope.isaSuperpowers, function(power) {
-            return !!mem[power];
-          });
+        var hasPower = function(power) {
+          return !!mem[power];
         };
+        scope.$watchCollection('isaSuperpowers', function(newSuperpowers) {
+          var predicate = _.isArray(newSuperpowers)
+            ? hasPower : function(condition, power) {
+              return condition ? hasPower(power) : true;
+            };
+          scope.denied = !_.every(newSuperpowers, predicate);
+        });
       }
-      if (_.has(attrs, 'isaLoading')) {
+      if (_.has(scope, 'isaLoading')) {
         var indicator = $compile(
           '<div class="form-group ng-hide text-center" ng-show="isaLoading">' +
           '<i class="fa fa-spin fa-spinner"></i>' +
