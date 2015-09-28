8d33c5bcb61a46ee717d00669566cc25f39d6963
Stephen Fortune <steve.fortune@icecb.com>
2015-09-28 11:47:32 +0100

    feature(address-book): Substance integration

--- client/addressBook/routes.ng.js
+++ client/addressBook/routes.ng.js
@@ -1,28 +1,31 @@
 'use strict';
 
 angular
-	.module('isa.addressbook')
-	.config(function($stateProvider) {
-		$stateProvider
-			.state('addressbook', {
-				parent: 'organisation',
-				url: '/addressbook',
-				templateUrl: 'client/addressBook/view/addressBook.ng.html',
-				controller: 'AddressBookController'
-			})
-			.state('addressbook.user', {
-				url: '/user/:id',
-				templateUrl: 'client/addressBook/view/viewUser.ng.html',
-				controller: 'AddressBookViewUserController'
-			})
-			.state('addressbook.organisation', {
-				url: '/organisation/:id',
-				templateUrl: 'client/addressBook/view/viewOrganisationAddress.ng.html',
-				controller: 'AddressBookViewOrganisationAddressController'
-			})
-			.state('addressbook.contact', {
-				url: '/contact/:id',
-				templateUrl: 'client/addressBook/view/viewContact.ng.html',
-				controller: 'AddressBookViewContactController'
-			});
-	});
+  .module('isa.addressbook')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('addressbook', {
+        parent: 'organisation',
+        url: '/addressbook',
+        templateUrl: 'client/addressBook/view/addressBook.ng.html',
+        controller: 'AddressBookController',
+        data: {
+          $subs: ['profileImages', 'contacts', 'organisationAddresses']
+        }
+      })
+      .state('addressbook.user', {
+        url: '/user/:id',
+        templateUrl: 'client/addressBook/view/viewUser.ng.html',
+        controller: 'AddressBookViewUserController'
+      })
+      .state('addressbook.organisation', {
+        url: '/organisation/:id',
+        templateUrl: 'client/addressBook/view/viewOrganisationAddress.ng.html',
+        controller: 'AddressBookViewOrganisationAddressController'
+      })
+      .state('addressbook.contact', {
+        url: '/contact/:id',
+        templateUrl: 'client/addressBook/view/viewContact.ng.html',
+        controller: 'AddressBookViewContactController'
+      });
+  });
