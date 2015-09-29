f008582988d51a6b37261e6dcc710d16bf60b05f
Michael Hamilton <michael_hamilton@teamstudio.com>
2015-09-29 15:10:13 -0400

    overview: make setup progress a more reactive.  [Finishes #103899966]

--- client/overview/orgSetupPanel.ng.js
+++ client/overview/orgSetupPanel.ng.js
@@ -10,11 +10,14 @@ function orgSetupPanelDirective() {
       org: '='
     },
     controller: function($scope) {
-      $scope.progressAmt = function() {
-        var doneAmt = $scope.org.setupDone ? $scope.org.setupDone.length : 0;
+      $scope._orgObj = angular.isString($scope.org) ? $scope.$meteorObject(Organisations, $scope.org, false) : org;
+      $scope.progressAmt = '0%';
+
+      $scope.$watch('_orgObj.setupDone', function(newVal) {
+        var doneAmt = angular.isArray(newVal) ? newVal.length : 0;
         var val = (doneAmt / 12.0) * 100.0;
-        return val + '%';
-      }
+        $scope.progressAmt = Math.round(val) + '%';
+      });
     }
   };
 }
