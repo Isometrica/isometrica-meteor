613fdc5ac8f0ef6912d1240ebcae1482ce1cd708
Michael Hamilton <michael_hamilton@teamstudio.com>
2015-10-12 08:53:21 -0400

    guidance: standard header uses profile to show/hide bar.

--- client/guidanceBar/guidanceBar.ng.js
+++ client/guidanceBar/guidanceBar.ng.js
@@ -20,15 +20,18 @@ app.directive('isaGuidanceBar', [ function() {
 			$scope.hideBar = false;
 
 			$scope.hideThis = function() {
+				if ($scope.textId) {
+					Meteor.users.update($rootScope.currentUser._id, {$addToSet: {'profile.hiddenGuidance': $scope.textId }});
+				}
 				$scope.hideBar = true;
-				$rootScope.isaGuidanceBarHidden = true;
 			};
 
 			if ($scope.textId) {
 				var t = SystemTexts.findOne( { textId : $scope.textId });
 				$scope.guidanceText = ( t ? t.contents : $scope.textId);
+				$scope.hideBar = -1 !== _.indexOf($rootScope.currentUser.profile.hiddenGuidance, $scope.textId);
 			}
-		
+
 		}
 
 	};
